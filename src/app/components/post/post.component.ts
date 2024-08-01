001850899292405dcbad787125660ba39367a2ab
Th0Gy <thaylonhaniel9@gmail.com>
2024-08-01 19:02:41 -0300

    handleDescription

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,6 @@
 import { userRoom } from './../../models/user.model';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CommentPost, Post } from '../../models/post.model';
 import { UsersDbService } from '../../services/userDb/users-db.service';
 import { PostsService } from '../../services/posts/posts.service';
@@ -22,15 +22,17 @@ export class PostComponent {
   ) {}
   like: boolean = true;
   comments: boolean = false;
-  archive: boolean = false;
   userPage: User = JSON.parse(localStorage.getItem('user')!);
   usersList!: userRoom[];
   commentsPost?: CommentPost[];
   errImage: boolean = false;
   comment?: CommentPost;
+  descriptionOpen: boolean = false;
 
   userName!: string;
   userPhoto!: string;
+
+  @ViewChild("descriptionContent") descriptionContent!: ElementRef<HTMLParagraphElement>;
 
   @Input() post!: Post;
 
@@ -98,8 +100,20 @@ export class PostComponent {
     }
   }
 
-  handleArchive(): void {
-    this.archive = !this.archive;
+  handleDescription(): void {
+    let descriptionStyle = this.descriptionContent.nativeElement.style; 
+    if(!this.descriptionOpen){
+      descriptionStyle.whiteSpace = "inherit";
+      descriptionStyle.width = "20rem";
+      descriptionStyle.height = "100%";
+      descriptionStyle.overflow = "scroll";
+    } else {
+      descriptionStyle.whiteSpace = "nowrap";
+      descriptionStyle.width = "9rem";
+      descriptionStyle.height = "inherit";
+      descriptionStyle.overflow = "hidden";
+    }
+    this.descriptionOpen = !this.descriptionOpen;
   }
 
   handleComments(): void {
@@ -118,11 +132,11 @@ export class PostComponent {
     }
     
     handleCloseModalComments(modal: boolean){
-    let bodyStyleOverflow = document.body.style;
+      let bodyStyleOverflow = document.body.style;
       this.comments = modal;
       this.comments
-        ? (bodyStyleOverflow.overflow = 'hidden')
-        : (bodyStyleOverflow.overflow = 'inherit');
+      ? (bodyStyleOverflow.overflow = 'hidden')
+      : (bodyStyleOverflow.overflow = 'inherit');
 
   }
 
